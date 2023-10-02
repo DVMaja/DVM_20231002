c1dfa746f01d2db866cec7c4812bb6d5a3662182
DVMaja <majadreilinger@gmail.com>
2023-10-02 09:12:40 +0200

    a pakli null, nem működik

--- src/probaprojekt/Pakli.java
+++ src/probaprojekt/Pakli.java
@@ -4,21 +4,33 @@ public class Pakli {
 
     private String[] pakli;
 
-    public Pakli() {
-        //this.pakli = pakli;
-    }
+//    public Pakli() {
+//        this.pakli = pakli;
+//    }
 
     private void osszerak() {
         String[] szin = {"P", "M", "Z", "S"};
         String[] szam = {"A", "K", "D", "J", "10", "9", "8"};
 
-        for (String string : szin) {
+//        for (String string : szin) {
+//            for (int szamDb = 0; szamDb < szam.length; szamDb++) {
+//                pakli[szamDb] = szin+ "_" + szam[szamDb];
+//            }
+//
+//        }
+        for (int szinDb = 0; szinDb < szin.length; szinDb++) {
             for (int szamDb = 0; szamDb < szam.length; szamDb++) {
-                pakli[szamDb] = szin + "_" + szam[szamDb];
+                pakli[szamDb] = szin[szinDb] + "_" + szam[szamDb];
+                
             }
-
+            
         }
 
+    }
+    public void kiIrat(){
+        for (int i = 0; i < pakli.length; i++) {
+            System.out.println(pakli[i]);
+        }
     }
 
     public void setPakli(String[] pakli) {
@@ -29,7 +41,7 @@ public class Pakli {
         return pakli;
     }
 
-    private void kever(int oszlop) {
+    public void kever(int oszlop) {
         switch (oszlop) {
             case 1:
                 for (int i = 0; i < 7; i++) {
@@ -62,7 +74,7 @@ public class Pakli {
 
     }
 
-    private void ezVolt() {
+    public void ezVolt() {
         System.out.println("Ez volt a gondolt lap" + pakli[11]);
     }
 }
--- src/probaprojekt/ProbaProjekt.java
+++ src/probaprojekt/ProbaProjekt.java
@@ -1,17 +1,34 @@
 package probaprojekt;
 
+import java.util.Scanner;
+
 public class ProbaProjekt {
 
-    
     public static void main(String[] args) {
         indit();
     }
 
     private static void indit() {
         Pakli pakli = new Pakli();
+        pakli.getPakli();
         for (int i = 0; i < 3; i++) {
-            
+            pakli.kiIrat();
+            int oszlop = oszlopBe();
+            pakli.kever(oszlop);
         }
     }
-    
+
+    private static int oszlopBe() {
+        Scanner sc = new Scanner(System.in);
+        int oszlop;
+
+        do {
+            System.out.print("Kérem adja meg melyik oszlopban van a választott kártya");
+            oszlop = sc.nextInt();
+            System.out.println("");
+        } while (oszlop > 3 && oszlop < 1);
+
+        return oszlop;
+    }
+
 }
